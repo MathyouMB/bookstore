dc3beeec44e1e1b04626f0eb7303d3980afae551
MathyouMB <matthewmacraebovell@bell.net>
2020-02-27 15:16:38 -0500

    first get fir books id works

--- SQL/db.sql
+++ SQL/db.sql
@@ -1,5 +1,38 @@
 create table book
-	(book_id		varchar(8), 
-	 title			varchar(50)
+	(book_id			varchar(8), 
+	 book_title			varchar(50),
+	 page_num			numeric(5,0),
+	 book_price 		decimal(7,2),
 	 primary key (book_id),
+	);
+
+insert into book values ('1','Book', '100', '2.99');
+/*
+
+Book needs
+author(s), 
+genre, 
+publisher, 
+
+number of pages, price
+
+/*
+
+create table user
+	(ID		varchar(8), 
+	 primary key (ID),
+	);
+
+
+create table collection
+	(collection_id				varchar(8), 
+	 collection_name			varchar(50),
+	 collection_description		varchar(50),
+	 primary key (collection_id),
+	);
+
+create table collection_book
+	(collection_id		varchar(8), 
+	 book_id			varchar(8)
+	 primary key (collection_id, book_id),
 	);
--- server/server.go
+++ server/server.go
@@ -12,22 +12,24 @@ import (
 )
 
 type Book struct {
-	Id       int      `json:"id"`
-	Title    string   `json:"title"`
+	Book_id       int      		`json:"id"`
+	Book_title    string   		`json:"title"`
+	Page_num      int     		`json:"page_num"`
+	Book_price    float32     	`json:"price"`
 }
 
 var db *sql.DB
 
 func GetBookHandler(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
-	rows, err := db.Query(`SELECT * FROM book WHERE id = $1`, id)
+	rows, err := db.Query(`SELECT * FROM book WHERE book_id = $1`, id)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
 	}
 	var book Book
 	defer rows.Close()
 	for rows.Next() {
-		err := rows.Scan(&book.Id, &book.Title)
+		err := rows.Scan(&book.Book_id, &book.Book_title, &book.Page_num, &book.Book_price)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -49,5 +51,6 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/books/{id}", GetBookHandler).Methods("GET")
 	http.Handle("/", r)
+	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", r)
 }
