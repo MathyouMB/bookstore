001e78ae2cae714497673345b8a3d002e3da5652
MathyouMB <matthewmacraebovell@bell.net>
2020-03-03 11:08:21 -0500

    added cart display with specs

--- client/src/components/CartPage.js
+++ client/src/components/CartPage.js
@@ -5,7 +5,39 @@ import Book from './BookContainer.js';
 function CartItem(props) {
     return (
         <div className="cart-item">
-           <Book book={props.item} user={props.user} ordered={true}/>
+           <Book book={props.book} user={props.user} ordered={true}/>
+           <div className="book-details-grid">
+                <div className="book-details-heading"><h3>Details & Spec</h3></div>
+                <hr></hr>
+                <br></br>
+                <div className="book-details-grid-row">
+                    <div className="book-details-grid-row-cell">Title</div>
+                    <div className="book-details-grid-row-cell">{props.book.Book_title}</div>
+                </div>
+                <div className="book-details-grid-row">
+                    <div className="book-details-grid-row-cell">Genre</div>
+                    <div className="book-details-grid-row-cell">{props.book.Book_genre}</div>
+                </div>
+                <div className="book-details-grid-row">
+                    <div className="book-details-grid-row-cell">ISBN</div>
+                    <div className="book-details-grid-row-cell">{props.book.ISBN}</div>
+                </div>
+                <div className="book-details-grid-row">
+                    <div className="book-details-grid-row-cell">Publisher</div>
+                    <div className="book-details-grid-row-cell">{props.book.Publisher.Publisher_name}</div>
+                </div>
+                <div className="book-details-grid-row">
+                    <div className="book-details-grid-row-cell">Authors</div>
+                    <div className="book-details-grid-row-cell">
+                        
+                        {   
+                            props.book.Authors.map( author => (
+                            <div>• {author.First_name} {author.Last_name}</div>
+                            ))        
+                        }  
+                    </div>
+                </div>
+            </div>
         </div>
       );
 }
@@ -33,7 +65,7 @@ function Cart(props) {
         loading ? "Loading..."
         :
         data.map( item => (
-            <CartItem item={item}/>
+            <CartItem book={item}/>
         ))
       
         }
